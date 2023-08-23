8cb0a3ac8cda603668f58f2f53425834502f6b8d
shikr <cristobal.rod0907@gmail.com>
2023-08-22 19:12:24 -0600

    feat: update border_width on border hover
    When the cursor is in a border update the width to
    indicate that the client can be resized. This causes that in some
    situations the border is always wide.

--- config/ruled.lua
+++ config/ruled.lua
@@ -1,5 +1,43 @@
 local ruled = require("ruled")
 local awful = require('awful')
+local gears = require('gears')
+local naughty = require 'naughty'
+
+local last_text = ''
+local notify = function (text)
+  if text == nil or last_text == text then
+    return
+  end
+  naughty.notify{text = text}
+  last_text = text
+end
+
+local function mouse_on_border(c, coords)
+  local geometry = c:geometry()
+  local cx, cy, cw, ch = geometry.x, geometry.y, geometry.width, geometry.height
+  local mx, my = coords.x, coords.y
+  local onborder = nil
+  if mx < cx or mx > (cx + cw + 1) or my < cy or my > (cy + ch + 1) then
+    return
+  elseif my > (cy + ch - 6) then
+    if mx < (cx + 30) then
+      onborder = "bottom_left"
+    elseif mx > (cx + cw - 30) then
+      onborder = "bottom_right"
+    else onborder = "bottom"
+    end
+  elseif my < (cy + 6) then
+    if mx < (cx + 30) then
+      onborder = "top_left"
+    elseif mx > (cx + cw - 30) then
+      onborder = "top_right"
+    else onborder = "top"
+    end
+  elseif mx < (cx + 4) then onborder = "left"
+  elseif mx > (cx + cw - 4) then onborder = "right"
+  end
+  return onborder
+end
 
 ruled.client.connect_signal("request::rules", function()
   -- All clients will match this rule.
@@ -10,8 +48,31 @@ ruled.client.connect_signal("request::rules", function()
       focus     = awful.client.focus.filter,
       raise     = true,
       screen    = awful.screen.preferred,
-      placement = awful.placement.no_overlap+awful.placement.no_offscreen
-    }
+      placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+    },
+    callback = function (c)
+      c:connect_signal('mouse::move', function ()
+        local position, cursor = mouse_on_border(c, mouse.coords())
+        if position ~= nil then
+          c.border_width = 2
+        else
+          c.border_width = 1
+        end
+        -- notify(position)
+      end)
+      c:connect_signal('mouse::leave', function ()
+        c.border_width = 1
+      end)
+      c:buttons(gears.table.join(
+        awful.button({}, 1, function ()
+          c:emit_signal("request::activate", "mouse_click", {raise = true})
+          local pos = mouse_on_border(c, mouse.coords())
+          if pos ~= nil then
+            awful.mouse.client.resize(c, pos)
+          end
+        end)
+      ))
+    end
   }
 
   -- Floating clients.
