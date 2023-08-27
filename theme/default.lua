e7e2d27b13f24d45607869ecaf37580eec8c1bc1
shikr <cristobal.rod0907@gmail.com>
2023-08-26 19:09:22 -0600

    feat: add default theme
    New file to manage non-color theme properties.

---
+++ theme/default.lua
@@ -0,0 +1,28 @@
+local gfs = require('gears.filesystem')
+local helpers = require('helpers')
+local themes_dir = gfs.get_themes_dir()
+local theme = dofile(themes_dir .. 'default/theme.lua')
+local config_dir = gfs.get_configuration_dir()
+local gcolor = require('gears.color')
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+local colors = require('theme.dark')
+
+
+colors(theme)
+
+theme.font_name = 'Sans '
+theme.font = theme.font_name .. dpi(8)
+
+theme.border_width = 0
+theme.border_radius = dpi(10)
+
+theme.menu_submenu_icon = gcolor.recolor_image(config_dir .. 'theme/assets/submenu.svg', theme.fg_normal)
+
+theme.tooltip_border_width = dpi(1)
+theme.tooltip_shape = helpers.ui.rrect(dpi(4))
+-- theme.tooltip_opacity = 0.95 -- defined in picom
+
+theme.icon_theme = nil
+
+return theme
