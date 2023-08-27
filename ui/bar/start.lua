ef4c81ec4cda057be76d3ca2683d8c2c7b45169d
shikr <cristobal.rod0907@gmail.com>
2023-08-27 00:35:12 -0600

    feat: redesign start button

--- ui/bar/start.lua
+++ ui/bar/start.lua
@@ -1,8 +1,33 @@
 local awful = require('awful')
+local wibox = require('wibox')
 local beautiful = require("beautiful")
+local dpi = beautiful.xresources.apply_dpi
 local main_menu = require('ui.menu')
+local gtable = require('gears.table')
+local helpers = require('helpers.ui')
 
-return awful.widget.launcher {
-  image = beautiful.awesome_icon,
-  menu = main_menu,
+local widget_menu = wibox.widget {
+  {
+    image = beautiful.awesome_icon,
+    resize = true,
+    clip_shape = helpers.rrect(6),
+    widget = wibox.widget.imagebox
+  },
+  margins = dpi(4),
+  widget = wibox.container.margin,
 }
+
+widget_menu:buttons(
+  gtable.join(
+    awful.button(
+      {},
+      1,
+      nil,
+      function ()
+        main_menu:toggle()
+      end
+    )
+  )
+)
+
+return widget_menu
