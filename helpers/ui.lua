612d119c3bc043d09340803187ee1f244a0e7c23
shikr <cristobal.rod0907@gmail.com>
2023-08-26 20:42:49 -0600

    feat: add hover effect helper

--- helpers/ui.lua
+++ helpers/ui.lua
@@ -8,4 +8,26 @@ function helpers.rrect(radius)
   end
 end
 
+---Update the cursor of a widget with hover.
+---@param widget table
+---@param cursor string hand2
+function helpers.add_hover_cursor(widget, cursor)
+  local old_cursor, old_widget
+
+  widget:connect_signal('mouse::enter', function ()
+    local w = mouse.current_wibox
+    if w then
+      old_cursor, old_widget = w.cursor, w
+      w.cursor = cursor
+    end
+  end)
+
+  widget:connect_signal('mouse::leave', function ()
+    if old_widget then
+      old_widget.cursor = old_cursor
+      old_widget = nil
+    end
+  end)
+end
+
 return helpers
