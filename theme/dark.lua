f1e55c640d3de942d06ddf05cc8286e3302e54f5
shikr <cristobal.rod0907@gmail.com>
2023-08-28 22:51:04 -0600

    feat: add background alt

--- theme/dark.lua
+++ theme/dark.lua
@@ -5,11 +5,22 @@ local themes_dir = gfs.get_themes_dir()
 local function load_theme(theme)
   theme.accent = '#FFFFFF'
 
-  theme.bg_color = '#101010'
-  theme.bg_normal = theme.bg_color .. 'E6'
-  theme.bg_focus = helpers.color.lighten(theme.bg_color, 0.25)
+  theme.background = '#101010'
+  theme.background_alt = helpers.color.lighten(theme.background, 0.1)
+  theme.background_alt2 = helpers.color.lighten(theme.background_alt, 0.1)
+
+  function theme.bg_hover(color)
+    return helpers.color.lighten(color, 0.05)
+  end
+
+  function theme.bg_color(color)
+    return helpers.color.change_opacity(color, 0.8)
+  end
+
+  theme.bg_normal = theme.bg_color(theme.background)
+  theme.bg_focus = theme.bg_color(theme.background_alt2)
   theme.bg_urgent = '#fb9c36'
-  theme.bg_minimize = helpers.color.lighten(theme.bg_color, 0.1)
+  theme.bg_minimize = theme.bg_color(theme.background_alt)
 
   theme.fg_normal = '#FFFFFF'
   theme.fg_focus = themes_dir.fg_normal
