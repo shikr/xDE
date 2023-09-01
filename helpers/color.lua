47e497e3f6dab34b9969ffd69ec7cd1ed55ef4a6
shikr <cristobal.rod0907@gmail.com>
2023-08-31 20:02:36 -0600

    fix: add alpha to color helpers

--- helpers/color.lua
+++ helpers/color.lua
@@ -4,21 +4,23 @@ local clip = require('helpers.math').clip
 local _color = {}
 
 function _color.lighten(color, amount)
-  local h, s, l = Color(color):hsl()
+  local h, s, l, a = Color(color):hsla()
 
   return tostring(Color {
     h = h,
     s = s,
-    l = clip(l + amount, 0, 1)
+    l = clip(l + amount, 0, 1),
+    a = a
   })
 end
 
 function _color.darken(color, amount)
-  local h, s, l = Color(color):hsl()
+  local h, s, l, a = Color(color):hsla()
   return tostring(Color {
     h = h,
     s = s,
-    l = clip(l - amount, 0, 1)
+    l = clip(l - amount, 0, 1),
+    a = a
   })
 end
 
