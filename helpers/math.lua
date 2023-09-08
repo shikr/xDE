4159650f9eafbc048cf401ae30caa4c6819d2a75
shikr <cristobal.rod0907@gmail.com>
2023-09-08 17:38:12 -0600

    feat: add percent helper

--- helpers/math.lua
+++ helpers/math.lua
@@ -4,4 +4,13 @@ function _math.clip(num, min_num, max_num)
   return math.max(math.min(num, max_num), min_num)
 end
 
+function _math.percent(percentage)
+  local negative = percentage < 0
+  percentage = math.abs(percentage)
+  if percentage > 1 then
+    percentage = percentage * .01
+  end
+  return negative and -percentage or percentage
+end
+
 return _math
