8e2595a006fbde774b4cfef872f5f9fdf53a087f
shikr <cristobal.rod0907@gmail.com>
2023-09-08 19:47:27 -0600

    feat: add a tooltip to clock

--- ui/bar/clock.lua
+++ ui/bar/clock.lua
@@ -1,12 +1,26 @@
+local awful = require('awful')
 local wibox = require('wibox')
 local button = require('ui.widgets.button')
 local dpi = require('beautiful.xresources').apply_dpi
 
 return function ()
-  return button(wibox.widget.textclock('%I:%M %p'), {
+  local clock = button(wibox.widget.textclock('%I:%M %p'), {
     margins = {
       left = dpi(3),
       right = dpi(3),
     }
   })
+
+  awful.tooltip{
+    objects = {
+      clock,
+    },
+    timeout = 60,
+    delay_show = 1,
+    timer_function = function ()
+      return os.date('%A %B %d %Y')
+    end
+  }
+
+  return clock
 end
