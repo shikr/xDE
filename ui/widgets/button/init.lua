d952620d36c8edabc432d28006773c3418ffddad
shikr <cristobal.rod0907@gmail.com>
2023-09-08 18:55:24 -0600

    feat: add default button

--- ui/widgets/button/init.lua
+++ ui/widgets/button/init.lua
@@ -1,4 +1,22 @@
-return {
-  color = require(... .. '.color'),
+local wibox = require('wibox')
+local color = require(... .. '.color')
+local dpi = require('beautiful.xresources').apply_dpi
+
+--- @type metatable
+local meta = {}
+
+-- default button
+function meta.__call(_, child, args)
+  args = args or {}
+  local margins = args.margins ~= nil and args.margins or dpi(3)
+  return color({
+    widget = wibox.container.margin,
+    margins = margins,
+    child
+  }, args)
+end
+
+return setmetatable({
+  color = color,
   size = require(... .. '.size'),
-}
+}, meta)
