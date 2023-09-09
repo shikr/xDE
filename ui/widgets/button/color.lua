1f18fea2da42203d1b21929804c8fe7954bc387e
shikr <cristobal.rod0907@gmail.com>
2023-09-09 14:52:48 -0600

    refactor: update color button
    Interpolates bg and bg_hover colors instead of updating
    color light.

--- ui/widgets/button/color.lua
+++ ui/widgets/button/color.lua
@@ -6,6 +6,7 @@ local Color = require('modules.lua-color')
 local function button(child, args)
   args = args or {}
   local bg = args.bg ~= nil and args.bg or beautiful.bg_normal
+  local bg_hover = args.bg_hover ~= nil and args.bg_hover or beautiful.bg_hover(bg)
 
   local widget = wibox.widget {
     widget = wibox.container.background,
@@ -16,29 +17,23 @@ local function button(child, args)
     child
   }
 
-  local h, s, light, a = Color(bg):hsla()
-  local _, _, hover_l = Color(beautiful.bg_hover(bg)):hsl()
+  local color = Color(bg)
+  local color_hover = Color(bg_hover)
 
   widget.animation = rubato.timed {
     duration = 0.2,
-    pos = light,
     awestore_compat = true,
     clamp_position = true,
     easing = rubato.easing.quadratic
   }
 
-  widget.animation:subscribe(function (l)
-      local color = Color({
-        h = h,
-        s = s,
-        l = l,
-        a = a
-      }):tostring()
-      widget.bg = color
-    end)
+  widget.animation:subscribe(function (pos)
+    local new_color = Color(color)
+    widget.bg = new_color:mix(color_hover, pos):tostring()
+  end)
 
   widget:connect_signal('mouse::enter', function ()
-    widget.animation:set(hover_l)
+    widget.animation:set(1)
   end)
 
   widget:connect_signal('mouse::leave', function ()
