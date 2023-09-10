4565e36a40e1b1c06067e54ffa9f8fd5c247cceb
shikr <cristobal.rod0907@gmail.com>
2023-09-09 18:44:03 -0600

    feat: add size animation to layoutbox

--- ui/bar/components/layoutbox.lua
+++ ui/bar/components/layoutbox.lua
@@ -11,6 +11,9 @@ return function (s)
         awful.button({ }, 4, function () awful.layout.inc(-1) end),
         awful.button({ }, 5, function () awful.layout.inc( 1) end),
       }
+    },
+    {
+      animations = { 'color', 'size' }
     }
   )
 end
