4a5e8f46512f0081637639128b90378619f834e8
shikr <cristobal.rod0907@gmail.com>
2023-10-02 17:00:17 -0600

    feat: use custom menu widget in main menu

--- ui/menu/init.lua
+++ ui/menu/init.lua
@@ -3,18 +3,41 @@ local beautiful = require("beautiful")
 local apps = require('config.apps')
 local format_cmd = require('helpers.run').format_cmd
 local hotkeys_popup = require("awful.hotkeys_popup")
+local menu = require('ui.widgets.menu')
 
-local awesome_menu = {
-  { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-  { "manual", format_cmd('man', 'awesome') },
-  { "edit config", format_cmd(apps.editor, awesome.conffile) },
-  { "restart", awesome.restart },
-  { "quit", function() awesome.quit() end },
-}
-
-return awful.menu {
-  items = {
-    { "awesome", awesome_menu, beautiful.awesome_icon },
-    { "open terminal", apps.terminal },
+return menu {
+  menu.button {
+    text = 'awesome',
+    image = beautiful.awesome_icon,
+    submenu = menu {
+      menu.button {
+        text = 'hotkeys',
+        callback = function ()
+          hotkeys_popup.show_help(nil, awful.screen.focused())
+        end
+      },
+      menu.button {
+        text = 'manual',
+        callback = format_cmd('man', 'awesome'),
+      },
+      menu.button {
+        text = 'edit config',
+        callback = format_cmd(apps.editor, awesome.conffile),
+      },
+      menu.button {
+        text = 'restart',
+        callback = awesome.restart,
+      },
+      menu.button {
+        text = 'quit',
+        callback = function ()
+          awesome.quit()
+        end,
+      },
+    },
+  },
+  menu.button {
+    text = 'open terminal',
+    callback = apps.terminal,
   },
 }
