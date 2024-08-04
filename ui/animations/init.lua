8e77b06c3df52d6acbc79a151295b7b4678001d2
shikr <cristobal.rod0907@gmail.com>
2024-08-03 21:51:38 -0600

    feat: add animate function

--- ui/animations/init.lua
+++ ui/animations/init.lua
@@ -1,51 +1,21 @@
-local subscribable = require('helpers.table').subscribable
 local events = require(... .. '.events')
 local animations = {
   color = require(... .. '.color'),
   size = require(... .. '.size')
 }
 
---- @type metatable
-local mt = {}
-
-local function subscribe(start, finish, handler)
-  start:subscribe(handler.start)
-  finish:subscribe(handler.finish)
-end
-
-function mt.__call(_, animation, widget, args)
-  for name, func in pairs(events) do
-    if animation[name] ~= nil then
-      local start = subscribable()
-      local finish = subscribable()
-      local ani = animation[name]
-      if type(ani) == 'string' then
-        subscribe(start, finish, animations[ani](widget, args))
-      elseif type(ani) == 'function' then
-        subscribe(start, finish, ani(widget, args))
-      else
-        for _, value in ipairs(ani) do
-          if type(value) == 'string' then
-            subscribe(start, finish, animations[value](widget, args))
-          else if type(value) == 'function' then
-              subscribe(start, finish, value(widget, args))
-            end
-          end
-        end
-      end
-      local handler = subscribable()
-      handler:subscribe(function (method)
-        if method == 'start' then
-          start:fire()
-        elseif method == 'finish' then
-          finish:fire()
-        end
-      end)
-      func(widget, handler)
+local function animate(widget, event, animation, fn, args)
+  if events[event] ~= nil and animations[animation] ~= nil then
+    if widget[event] == nil then
+      events[event](widget)
     end
-  end
 
-  return widget
+    return animations[animation](fn, args)
+  end
 end
 
-return setmetatable(animations, mt)
+return {
+  animations = animations,
+  events = events,
+  animate = animate
+}
